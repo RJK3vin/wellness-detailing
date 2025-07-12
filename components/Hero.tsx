cc6cb4574e7d594e7d7372b4508a2f82d504db85
Kevin Ritthaworn <rj.ritthaworn@icloud.com>
2025-07-12 01:21:17 -0400

    skip warning comment

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import type { RefObject } from 'react';
 import Link from 'next/link';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface HeroProps {
   scrollToRef: RefObject<HTMLDivElement | null>;
 }
